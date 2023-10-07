8736c8a55f11582d0e6b396052fdd59a0a45c4e4
anshu129 <114096201+anshu129@users.noreply.github.com>
2023-10-07 21:08:40 +0530

    defining function

--- app.py
+++ app.py
@@ -1,8 +1,34 @@
-from flask import Flask,jsonify
+from flask import Flask , jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+@app.route('/search/<string:query>')
+def search_product(query):
+    # Your code to search for products based on the 'query' parameter
+    # You will implement web scraping or API calls to fetch product data
+
+    # Example response (replace with actual data)
+    results = [
+        {"name": "Product 1", "price": 100},
+        {"name": "Product 2", "price": 150},
+        # Add more products here
+    ]
+
+    return jsonify(results)
+@app.route('/compare/<string:product_id_1>/<string:product_id_2>')
+def compare_products(product_id_1, product_id_2):
+    # Your code to compare products based on their IDs
+    # You will implement logic to compare products and return the comparison
+
+    # Example response (replace with actual data)
+    comparison = {
+        "product_1": {"name": "Product 1", "price": 100},
+        "product_2": {"name": "Product 2", "price": 150},
+        "result": "Product 1 is cheaper",
+    }
+
+    return jsonify(comparison)
 if __name__ =="__main__":
     app.run(debug=True)
